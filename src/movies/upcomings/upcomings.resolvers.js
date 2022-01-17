05cfcce29e2fa8a42af477292cb05b5ecd39169d
WONI <codeliner96@fixelsoft.com>
2022-01-17 16:12:46 +0900

    Implement movies upcomings.

---
+++ src/movies/upcomings/upcomings.resolvers.js
@@ -0,0 +1,45 @@
+import { getDefaultArguments, getMovieRequest } from "../../utils/axiosUtils";
+
+export default {
+  Query: {
+    movieUpcomings: async (_, { page = 1, language, region }) => {
+      try {
+        const request = getMovieRequest();
+        const args = getDefaultArguments();
+        args.append("page", page);
+        if (language) args.append("language", language);
+        if (region) args.append("region", region);
+
+        const { status, statusText, data } = await request.get(
+          `movie/upcoming?${args.toString()}`
+        );
+
+        // Response is invalid?
+        if (status !== 200) {
+          console.error("[movieUpcomings]", statusText);
+          return {
+            ok: false,
+            error: {
+              code: status,
+              message: statusText,
+            },
+          };
+        }
+
+        return {
+          ok: true,
+          data,
+        };
+      } catch (error) {
+        console.error("[movieUpcomings]", error);
+        return {
+          ok: false,
+          error: {
+            code: 500,
+            message: error?.message,
+          },
+        };
+      }
+    },
+  },
+};
---
+++ src/movies/upcomings/upcomings.typeDefs.js
@@ -0,0 +1,39 @@
+import { gql } from "apollo-server-core";
+
+export default gql`
+  type MovieUpcomingDates {
+    maximum: String
+    minimum: String
+  }
+
+  type MovieUpcoming {
+    poster_path: String
+    adult: Boolean
+    overview: String
+    release_date: String
+    genre_ids: [Int]
+    id: Int
+  }
+
+  type MovieUpcomingData {
+    page: Int
+    results: [MovieUpcoming]
+    dates: MovieUpcomingDates
+    total_pages: Int
+    total_results: Int
+  }
+
+  type MovieUpcomingResponse {
+    ok: Boolean!
+    error: SimpleError
+    data: MovieUpcomingData
+  }
+
+  type Query {
+    movieUpcomings(
+      page: Int
+      language: String
+      region: String
+    ): MovieUpcomingResponse!
+  }
+`;
