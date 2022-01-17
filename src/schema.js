f765a50c4c2f43d17aa4d788ceaddb194137fbf1
WONI <codeliner96@fixelsoft.com>
2022-01-17 11:04:37 +0900

    Initialize apollo server.

---
+++ src/schema.js
@@ -0,0 +1,13 @@
+import path from "path";
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
+
+// Load typeDefs.
+export const typeDefs = mergeTypeDefs(
+  loadFilesSync(path.join(__dirname, "/**/*.typeDefs.js"))
+);
+
+// Load resolvers.
+export const resolvers = mergeResolvers(
+  loadFilesSync(path.join(__dirname, "/**/*.resolvers.js"))
+);
--- src/server.js
+++ src/server.js
@@ -1,1 +1,60 @@
+require("dotenv").config();
 
+import http from "http";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { graphqlUploadExpress } from "graphql-upload";
+import logger from "morgan";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import { typeDefs, resolvers } from "./schema";
+
+// Declared server port.
+const PORT = process.env.PORT;
+
+runServer();
+
+/**
+ * Methods.
+ */
+
+/**
+ * ### Run server method.
+ */
+async function runServer() {
+  // Declared express app.
+  const app = express();
+  // Declared http server.
+  const httpServer = http.createServer(app);
+
+  // Declared apollo server.
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async (ctx) => {},
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await apolloServer.start();
+
+  // Apply each middleware.
+  app.use(graphqlUploadExpress());
+  app.use(
+    process.env.NODE_ENV !== "production" ? logger("dev") : logger("common")
+  );
+  app.use("/static", express.static("uploads"));
+
+  // Apply middleware into apollo server.
+  apolloServer.applyMiddleware({
+    app,
+    path: "/graphql",
+  });
+
+  await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+  if (process.env.NODE_ENV === "production") {
+    console.info(`Server running.`);
+  } else {
+    console.info(
+      `Server running at http://localhost:${PORT}${apolloServer.graphqlPath}.`
+    );
+  }
+}
