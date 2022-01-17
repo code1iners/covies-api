984cb777032efea886c65e5acbc0d60db9c8e524
WONI <codeliner96@fixelsoft.com>
2022-01-17 16:21:39 +0900

    Implement movies top rated.

---
+++ src/movies/topRated/topRated.resolvers.js
@@ -0,0 +1,49 @@
+import { getDefaultArguments, getMovieRequest } from "../../utils/axiosUtils";
+
+export default {
+  Query: {
+    movieTopRated: async (_, { page = 1, language, region }) => {
+      try {
+        // Init request.
+        const request = getMovieRequest();
+
+        // Init arguments.
+        const args = getDefaultArguments();
+        args.append("page", page);
+        if (language) args.append("language", language);
+        if (region) args.append("region", region);
+
+        // Data fetch.
+        const { status, statusText, data } = await request.get(
+          `movie/top_rated?${args.toString()}`
+        );
+
+        // Response is invalid?
+        if (status !== 200) {
+          console.error("[movieTopRated]", statusText);
+          return {
+            ok: false,
+            error: {
+              code: status,
+              message: statusText,
+            },
+          };
+        }
+
+        return {
+          ok: true,
+          data,
+        };
+      } catch (error) {
+        console.error("[movieTopRated]", error);
+        return {
+          ok: false,
+          error: {
+            code: 500,
+            message: error?.message,
+          },
+        };
+      }
+    },
+  },
+};
---
+++ src/movies/topRated/topRated.typeDefs.js
@@ -0,0 +1,41 @@
+import { gql } from "apollo-server-core";
+
+export default gql`
+  type MovieTopRated {
+    poster_path: String
+    adult: Boolean
+    overview: String
+    release_date: String
+    genre_ids: [Int]
+    id: Int
+    original_title: String
+    original_language: String
+    title: String
+    backdrop_path: String
+    popularity: Float
+    vote_count: Int
+    video: Boolean
+    vote_average: Float
+  }
+
+  type MovieTopRatedData {
+    page: Int
+    results: [MovieTopRated]
+    total_results: Int
+    total_pages: Int
+  }
+
+  type MovieTopRatedResponse {
+    ok: Boolean!
+    error: SimpleError
+    data: MovieTopRatedData
+  }
+
+  type Query {
+    movieTopRated(
+      page: Int
+      language: String
+      region: String
+    ): MovieTopRatedResponse!
+  }
+`;
